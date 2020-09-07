27760b34690fb1f8efba5831dbced3b255923b6f
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-09-07 16:55:21 -0300

    adding message box

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import "./Feed.css";
 import StoryReel from './StoryReel';
+import MessageSender from './MessageSender';
 
 const Feed: React.FC = () => {
   return (
     <div className="feed">
       <StoryReel />
+      <MessageSender/>
     </div>
   )
 }
---
+++ src/MessageSender.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import './MessageSender.css';
+import { Avatar } from '@material-ui/core';
+import VideocamIcon from '@material-ui/icons/Videocam';
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+
+const MessageSender: React.FC = () => {
+  const [input, setInput] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+
+  function handleSubmit(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+
+    setInput('');
+    setImageUrl('');
+  }
+
+  return (
+    <div className="messageSender">
+      <div className="messageSender__top">
+        <Avatar />
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className="messageSender__input"
+            placeholder={`what's on your mind`}
+          />
+          <input
+            value={imageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            placeholder="image URL (Optional)"
+          />
+
+          <button onClick={handleSubmit} type="button">
+            Hidden Submit
+          </button>
+        </form>
+      </div>
+
+      <div className="messageSender__botton">
+        <div className="messageSender__option">
+          <VideocamIcon style={{ color: 'red' }} />
+          <h3>Live Video</h3>
+        </div>
+
+        <div className="messageSender__option">
+          <PhotoLibraryIcon style={{ color: 'green' }} />
+          <h3>Photo/Video</h3>
+        </div>
+
+        <div className="messageSender__option">
+          <InsertEmoticonIcon style={{ color: 'orange' }} />
+          <h3>Feeling/Activity</h3>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MessageSender;
