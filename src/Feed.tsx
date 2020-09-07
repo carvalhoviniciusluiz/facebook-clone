66096f526ed6af040ad296935ddbed9cffd08265
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-09-07 19:10:17 -0300

    adding post

--- src/Feed.tsx
+++ src/Feed.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import "./Feed.css";
 import StoryReel from './StoryReel';
 import MessageSender from './MessageSender';
+import Post from './Post';
+import "./Feed.css";
 
 const Feed: React.FC = () => {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender/>
+
+      <Post
+        profilePic="https://avatars0.githubusercontent.com/u/22005684?s=460&u=b950a6cf4e12e66a7187f33407abb88389e5250d&v=4"
+        message="WOW this works!"
+        timestamp="This is a timestamp.."
+        username="carvalhoviniciusluiz"
+        image="https://www.wernerpatels.com/wp-content/uploads/2020/06/social-media.jpg"
+      />
+      <Post
+        profilePic="https://avatars0.githubusercontent.com/u/22005684?s=460&u=b950a6cf4e12e66a7187f33407abb88389e5250d&v=4"
+        message="WOW this works!"
+        timestamp="This is a timestamp.."
+        username="carvalhoviniciusluiz"
+        image="https://www.wernerpatels.com/wp-content/uploads/2020/06/social-media.jpg"
+      />
+      <Post
+        profilePic="https://avatars0.githubusercontent.com/u/22005684?s=460&u=b950a6cf4e12e66a7187f33407abb88389e5250d&v=4"
+        message="WOW this works!"
+        timestamp="This is a timestamp.."
+        username="carvalhoviniciusluiz"
+        image="https://www.wernerpatels.com/wp-content/uploads/2020/06/social-media.jpg"
+      />
     </div>
   )
 }
---
+++ src/Post.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import "./Post.css";
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import NearMeIcon from '@material-ui/icons/NearMe';
+import { ExpandMoreOutlined } from '@material-ui/icons';
+
+interface PostProps {
+  profilePic?: string;
+  image?: string;
+  username?: string;
+  timestamp?: string;
+  message?: string;
+}
+
+const Post: React.FC<PostProps> = ({
+  profilePic,
+  image,
+  username,
+  timestamp,
+  message
+}) => {
+  return (
+    <div className="post">
+      <div className="post__top">
+        <Avatar src={profilePic} className="post__avatar" />
+        <div className="post__topInfo">
+          <h3>{username}</h3>
+          <p>Timestamp..</p>
+        </div>
+      </div>
+
+      <div className="post__bottom">
+        <p>{message}</p>
+      </div>
+
+      <div className="post__image">
+        <img src={image} alt=""/>
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlined />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post;
