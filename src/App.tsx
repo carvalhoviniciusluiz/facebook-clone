6c95bb39e26eb06140c0a33d90a5b51ed19a4af7
Carvalho, Vinicius Luiz <carvalho.viniciusluiz@gmail.com>
2020-09-20 18:07:22 -0300

    adding widgets

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Widgets from './Widgets';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
       <div className="app__body">
         <Sidebar />
         <Feed />
-        {/* widgets */}
+        <Widgets />
       </div>
     </div>
   );
---
+++ src/Widgets.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Widgets: React.FC = () => {
+  return (
+    <div className="widgets">
+      <iframe
+        title="widgets"
+        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FNarutoUzumakirajada%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId"
+        width="340"
+        height="100%"
+        style={{ border: 'none', overflow: 'hidden' }}
+        scrolling="no"
+        frameBorder="none"
+        allowTransparency
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+}
+
+export default Widgets;
